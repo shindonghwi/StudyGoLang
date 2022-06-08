2550b648823de19b8cb56cc5925a7a3f482aaf74
shindonghwi <ehdgnl8940@naver.com>
2022-06-08 22:39:53 +0900

    feat: 반복문 기초사용

--- test.go
+++ test.go
@@ -1,27 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
-	var a = 5.0
-	b := 4.0
-
-	println(a, b)
-
-	a++
-	b--
-
-	println(&a, &b)
-
-	var scanData1 int
-	var scanData2 int
-	var scanData3 int
-
-	fmt.Scan(&scanData1, &scanData2, &scanData3)
-
-	println("scanData: ", scanData1, scanData2, scanData3)
+	for i := 0; i < 5; i++ {
+		println("data: ", i)
+	}
 
 }
