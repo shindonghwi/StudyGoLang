27ec6c91fcd84c546536ec95e87fe46b5b37eb56
shindonghwi <ehdgnl8940@naver.com>
2022-06-08 22:21:32 +0900

    feat: go int, string 생성하는 법

--- test.go
+++ test.go
@@ -3,17 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
 
-	/**
-	.go 파일을 실행하는 3가지 방법
+	var awesome int = 3
+	var cool string = "hello world"
 
-	go run // 컴파일 후 exe 파일을 만들지 않고 바로 실행
-
-	go build // 컴파일 후 exe 파일을 만들어 같은 폴더에 생성
-
-	go install // 컴파일 후 exe 파일을 상위의 bin 폴더에 생성
-
-	*/
+	fmt.Println(awesome, cool)
 
 }
