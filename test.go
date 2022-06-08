92ffbde4da847a1debad8fecc6ff6791b4f06662
shindonghwi <ehdgnl8940@naver.com>
2022-06-08 22:11:52 +0900

    feat: .go 파일을 실행하는 3가지 방법

--- test.go
+++ test.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	/**
+	.go 파일을 실행하는 3가지 방법
+
+	go run // 컴파일 후 exe 파일을 만들지 않고 바로 실행
+
+	go build // 컴파일 후 exe 파일을 만들어 같은 폴더에 생성
+
+	go install // 컴파일 후 exe 파일을 상위의 bin 폴더에 생성
+
+	*/
+
 }
